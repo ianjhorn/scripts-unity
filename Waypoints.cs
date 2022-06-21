c5d151b17c72cdfcb8d209c6b730356a156e5003
ianjhorn <107939032+ianjhorn@users.noreply.github.com>
2022-06-21 17:50:29 +0200

    Update Waypoints.cs

--- Waypoints.cs
+++ Waypoints.cs
@@ -4,7 +4,7 @@ public class Waypoints : MonoBehaviour
 {
     public static Transform[] points;
 
-    void Awake () // maakt een array van alle waypoints
+    void Waypoint () // maakt een array van alle waypoints
     {
         points = new Transform[transform.childCount]; 
         for (int i = 0; i < points.Length; i++)
